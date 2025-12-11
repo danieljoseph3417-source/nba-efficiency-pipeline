c8a5d22f38731b19714fb7ba0869740b13ab1a81
danieljoseph3417-source <danieljoseph3417@gmail.com>
2025-12-11 18:01:06 -0500

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,18 +1,14 @@
 import pandas as pd
 import random
 
-# 1. We create a "Fake" Dataset simulating a messy CSV file
+#  "Fake" Dataset simulating a messy CSV file
 raw_data = {
     "Player": ["LeBron James", "Steph Curry", "Kevin Durant", "LeBron James", "Giannis", "Luka", "Jokic", "Embiid", "Tatum", "Unknown_Rookie"],
     "Team": ["Lakers", "Warriors", "Suns", "Lakers", "Bucks", "Mavs", "Nuggets", "Sixers", "Celtics", "Jazz"],
     "Points": [28.9, 29.4, 29.1, 28.9, 31.1, 32.4, 24.5, 33.1, 30.1, 5.0],
     "Salary": [47000000, 48000000, 44000000, 47000000, 42000000, 37000000, None, 35000000, 30000000, 1000000]
-    # Notice the duplicate LeBron and the missing (None) Salary for Jokic
 }
 
 df = pd.DataFrame(raw_data)
-
-# 2. Save it as a raw CSV file
 df.to_csv("nba_raw_data.csv", index=False)
-
 print("✅ Source file 'nba_raw_data.csv' has been created.")
